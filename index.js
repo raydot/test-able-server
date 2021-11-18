206c2e55efb63af695dbab13e269d81a2d02148b
Dave Kanter <moatgoat@hotmail.com>
2021-11-18 12:32:51 -0800

    Added Authorization to headers

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(pino);
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
+  res.header('Access-Control-Allow-Headers', 'Content-type, Authorization');
   next();
 });
 
