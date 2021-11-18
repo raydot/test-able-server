f095b4a1f20004e2b1acdcf3708859e19bc57611
Dave Kanter <moatgoat@hotmail.com>
2021-11-18 11:50:40 -0800

    Added cors plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const { videoToken } = require('./tokens');
@@ -28,19 +29,19 @@ const sendTokenResponse = (token, res) => {
   );
 };
 
-app.get('/api/greeting', (req, res) => {
+app.get('/api/greeting', cors(), (req, res) => {
   const name = req.query.name || 'World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.get('/video/token', (req, res) => {
+app.get('/video/token', cors(), (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
-app.post('/video/token', (req, res) => {
+app.post('/video/token', cors(), (req, res) => {
   const identity = req.body.identity;
   const room = req.body.room;
   const token = videoToken(identity, room, config);
