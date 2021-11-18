2cf5f7036b9e0813cecea44241f45f4224719ee0
Dave Kanter <moatgoat@hotmail.com>
2021-11-18 15:21:40 -0800

    Working it from /api/greeting

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const sendTokenResponse = (token, res) => {
   );
 };
 
-app.get('/api/greeting', cors(), (req, res) => {
+app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
