f18d78b3fec6f4546d80a4820766c490b7824a01
Dave Kanter <moatgoat@hotmail.com>
2021-11-18 13:10:30 -0800

    Changed the sendTokenResponse function

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
 });
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
   res.send(
     JSON.stringify({
       token: token.toJwt(),
